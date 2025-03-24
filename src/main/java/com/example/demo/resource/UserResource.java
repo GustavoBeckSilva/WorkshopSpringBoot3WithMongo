8130b0a73fc315f8387dfad8d768e216401bf087
GustavoBeckSilva <gustavogbks@gmail.com>
2025-03-24 15:49:54 -0300

    Creating User resource, repository and service

--- src/main/java/com/example/demo/resource/UserResource.java
+++ src/main/java/com/example/demo/resource/UserResource.java
@@ -1,31 +1,29 @@
 package com.example.demo.resource;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.User;
+import com.example.demo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria Silva", "maria@gmail.com");
-		User joao = new User("2", "João Silva", "joao@gmail.com");
-
-		List<User> list = new ArrayList<>();
 		
-		list.add(joao);
-		list.add(maria);
-		
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+		
 	}
 
 }
---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+}
