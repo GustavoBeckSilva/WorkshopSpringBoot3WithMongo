634bd44ffeb42f8e43e2639bf7cc4d27fa7c0687
GustavoBeckSilva <gustavogbks@gmail.com>
2025-03-24 17:10:41 -0300

    Creating the getById method

--- src/main/java/com/example/demo/resource/UserResource.java
+++ src/main/java/com/example/demo/resource/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,15 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDTO);
 		
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		
+		User user = service.findById(id);
+		UserDTO userDTO = new UserDTO(user);
+		return ResponseEntity.ok().body(userDTO);
+		
+	}
+	
 
 }
